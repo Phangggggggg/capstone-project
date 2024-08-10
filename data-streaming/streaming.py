bd323d6d9e128d6a860aaf2828c26e3d01d3dcd3
Phangggggggg <pith.laohavirojana@gmail.com>
2024-08-10 17:13:03 +0700

    add stock websocket

--- data-streaming/streaming.py
+++ data-streaming/streaming.py
@@ -1,4 +1,5 @@
 import json
+import threading
 import time
 from functions.producer import KafkaProducer
 from functions.finhub_api import FinnhubClient
@@ -10,6 +11,8 @@ from functools import lru_cache
 from database.last_fetch import FetchTracker,setup_tracking_database,get_last_fetch_date,update_last_fetch_date
 import logging
 import schedule
+import websocket
+
 
 
 logging.basicConfig(level=logging.INFO)
@@ -17,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 @lru_cache(maxsize=None)
 def load_symbol_lists():
-    return pd.read_csv('data-streaming/data/nasdaq-listed-symbols.csv')['Symbol'].to_list()
+    return pd.read_csv('data-streaming/data/nasdaq_10.csv',encoding='utf-8')['Ticker'].to_list()
 
 symbol_lists = load_symbol_lists()
 
@@ -49,32 +52,61 @@ def fetch_visa():
         except Exception as e:
             logger.error(f"Error processing symbol {symbol}: {e}")
 
-def fetch_stock():
-    for symbol in symbol_lists:
-        data = finhub_client.get_stock_data(symbol=symbol)
-        if data:
-            stock_producer.produce_message(key=symbol,message=data)
+
+def on_message(ws, message):
+    message = json.loads(message)
+    if message and message['type'] == 'trade':
+        data = message['data']
+        if data and len(data) > 0:
+            for info in data:
+                stock_producer.produce_message(key=data['s'],value=info)
             stock_producer.flush()
+            logger.info(f"{stock_producer.get_delivered_count()} messages were produced to topic {STOCK_TOPIC}!")
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    for symbol in symbol_lists:
+        listen_message = {"type": "subscribe", "symbol": symbol}
+        ws.send(json.dumps(listen_message))
+    
+def fetch_stock():
+    ws = websocket.WebSocketApp(f"wss://ws.finnhub.io?token={FINHUB_API_KEY}",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+
                 
 if __name__ == '__main__':
     load_dotenv()
-    API_KEY = os.environ.get("API_KEY")
+    FINHUB_API_KEY = os.environ.get("FINHUB_API_KEY")
     VISA_TOPIC = os.environ.get("VISA_TOPIC")
     STOCK_TOPIC = os.environ.get("STOCK_TOPIC")
     CONFIG_FILE = os.environ.get("CONFIG_FILE")
     START_DATE = "2023-08-10"
     END_DATE = datetime.now().date().strftime('%Y-%m-%d')
     
-    finhub_client = FinnhubClient(api_key=API_KEY)
+    finhub_client = FinnhubClient(api_key=FINHUB_API_KEY)
+    
     visa_producer = KafkaProducer(config_file=CONFIG_FILE,topic=VISA_TOPIC)
     stock_producer = KafkaProducer(config_file=CONFIG_FILE,topic=STOCK_TOPIC)
     
     setup_tracking_database()
-    
-    schedule.every().day.at("14:06").do(fetch_visa)
+    websocket.enableTrace(True)
+    fetch_stock()
+    # print(1)
+    # stock_thread = threading.Thread(target=fetch_stock, daemon=True)
+    # stock_thread.start()
+    # # schedule.every(2).minutes.do(lambda: threading.Thread(target=fetch_visa).start())
+    # schedule.every(60).minutes.do(fetch_visa)
 
-    schedule.every(1).minutes.do(fetch_stock)
-
-    while True:
-        schedule.run_pending()
-        time.sleep(10) 
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(1)
